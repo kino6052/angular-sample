416bc4878b7d8724222793bfd030473498bf4849
kino6052 <kino6052@colorado.edu>
2016-02-01 15:18:43 -0700

    Added Working Call-log Form with POST Requests

--- client/app/call-tracker/call-tracker.controller.js
+++ client/app/call-tracker/call-tracker.controller.js
@@ -1,34 +1,27 @@
 'use strict';
 
 angular.module('oxhnApp')
-  .controller('CallTrackerCtrl', function ($scope) {
-    $scope.message = 'Hello';
-    console.log("test");
-    var _selected;
-    $scope.selected = undefined;
-    $scope.states = ["TV","Friend","Other"];
-    // Any function returning a promise object can be used to load values asynchronously
-    
-    $scope.help = 'Change';  
-    $scope.outcome = 'Scheduled';
-    $scope.checkResults = [];
-    
-
-    $scope.save = function() {
-        $scope.$broadcast('show-errors-check-validity');
-        console.log({
-        "firstName": "$scope.user.firstName"
-        });
-        if ($scope.userForm.$invalid) { return; }
-        console.log({
-        "firstName": $scope.user.firstName
-        });
-        // code to add the user
+  .controller('CallTrackerCtrl', function ($scope, $http) {
+      
+    // Model
+    $scope.user = {
+        callType: 'Change',
+        outcome: 'Scheduled',
+        textarea: ''
     };
     
-    $scope.reset = function() {
-        $scope.$broadcast('show-errors-reset');
-        $scope.user = { name: '', email: '' };
-        $scope.phoneVal = '';
+    // Save User
+    $scope.save = function() {
+        $scope.$broadcast('show-errors-check-validity');
+        if ($scope.userForm.$invalid) { return; }
+        console.log('Control reached here');
+        $http.post('/api/call-tickets', $scope.user).then(
+            function(data){
+                console.log(data);
+            },
+            function(error){
+                console.log(error);
+            }
+        );
     };
   });
