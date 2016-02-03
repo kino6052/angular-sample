58652e0939afa6f769148abbda17b8dad396e256
kino6052 <kino6052@colorado.edu>
2016-02-03 11:29:32 -0700

    Fixed Validation Issues

--- client/components/showErrors/showErrors.directive.js
+++ client/components/showErrors/showErrors.directive.js
@@ -6,51 +6,55 @@ angular.module('oxhnApp')
     restrict: 'A',
     require: '^form',
     link: function (scope, el, attrs, formCtrl) {
+      var findClosestParentThatHasInvalidProperty = function(element){
+          if (!formCtrl[element.attr('name')] || formCtrl[element.attr('name')].$invalid === "undefined") return findClosestParentThatHasInvalidProperty(element.parent());
+          else {
+              return element
+          }
+      }
+      // find the first form-control parent
+      var findFormControl = function(mode, element, invalid) {
+          if (element.hasClass("form-group")){
+              if (mode == "toggle"){
+                element.toggleClass('has-success', !invalid);
+                element.toggleClass('has-error', invalid);
+              }
+              if (mode == "reset"){
+                element.removeClass('has-error');
+                element.removeClass('has-success');
+              }
+          }
+          else {
+              findFormControl(mode, element.parent(), invalid);
+          }
+      } 
       // find the text box element, which has the 'name' attribute
       var inputEl   = el[0].querySelector("[name]");
       // convert the native text box element to an angular element
       var inputNgEl = angular.element(inputEl);
       // get the name on the text box so we know the property to check
-      // on the form controller
+
       var inputName = inputNgEl.attr('name');
       
       inputNgEl.bind('blur', function() {
-        el.removeClass('has-error');
-        el.removeClass('has-success');
-        el.parent().removeClass('has-error');
-        el.parent().removeClass('has-success');
-        var invalid;
-        if (formCtrl[inputName]){
-            
-            invalid = formCtrl[inputName].$invalid
-            el.toggleClass('has-success', !invalid);
-            el.toggleClass('has-error', invalid);
-        }
-        else {
-            
-            invalid = inputNgEl.$invalid;
-            el.parent().toggleClass('has-success', !invalid);
-            el.parent().toggleClass('has-error', invalid);
-        }    
-        
+        var elementWithValidation = findClosestParentThatHasInvalidProperty(inputNgEl);
+        console.log(inputNgEl.attr('class'));
+        findFormControl('reset', elementWithValidation);
+        var invalid = formCtrl[elementWithValidation.attr('name')].$invalid; 
+            findFormControl('toggle', elementWithValidation, invalid) 
       });
       
       scope.$on('show-errors-check-validity', function() {
-        el.removeClass('has-error');
-        el.removeClass('has-success');
-        el.parent().removeClass('has-error');
-        el.parent().removeClass('has-success');
+        findFormControl('reset', el);
         var invalid;
         if (formCtrl[inputName]){
             invalid = formCtrl[inputName].$invalid
-            el.toggleClass('has-error', invalid);
-            el.toggleClass('has-success', !invalid);
+            findFormControl('toggle', el, invalid) 
             
         }
         else {
             invalid = inputNgEl.$invalid;
-            el.parent().toggleClass('has-error', invalid);
-            el.parent().toggleClass('has-success', invalid);
+            findFormControl('toggle', el, invalid) 
         }   
       });
       
