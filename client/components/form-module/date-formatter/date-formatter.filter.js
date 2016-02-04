5f19ded549c2c1fef06611db0b4def11e03a8e23
kino6052 <kino6052@colorado.edu>
2016-02-04 16:10:27 -0700

    Debugged form.module Issues

--- client/components/form-module/date-formatter/date-formatter.filter.js
+++ client/components/form-module/date-formatter/date-formatter.filter.js
@@ -1,4 +1,4 @@
-angular.module('form.module', []).filter('dateFilter', function() {
+angular.module('form.module').filter('dateFilter', function() {
   return function(input) {
     return input?moment.utc(input).format('MMMM Do YYYY, h:mm:ss a'):'';
   };
