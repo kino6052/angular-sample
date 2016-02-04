b83e8e2403e8ba9b14a25bf632cfe0acd9f7c08d
kino6052 <kino6052@colorado.edu>
2016-02-04 15:22:18 -0700

    Added Icons and Zebra Striping + Buttons instead of Radio

---
+++ client/components/table/table-filter/table-filter.filter.js
@@ -0,0 +1,23 @@
+angular.module('form.module', []).filter('tableFilter', function() {
+  return function(input) {
+    switch (input) {
+        case 'New Patients':
+            return 'glyphicon glyphicon-user';
+        case '1st Treatment':
+            return 'glyphicon glyphicon-grain'
+        case 'Treatments':
+            return 'glyphicon glyphicon-check'
+        case 'ABI':
+            return 'glyphicon glyphicon-grain'
+        case 'Biopsy':
+            return 'glyphicon glyphicon-search'
+        case 'No Shows':
+            return 'glyphicon glyphicon-eye-close'
+        case 'Canceled':
+            return 'glyphicon glyphicon-remove'
+        default:
+            break;
+    }
+    return input;
+  };
+});
