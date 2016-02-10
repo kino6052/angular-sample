03d844dc05ab6d1b9b3a46aaa00f41aa16b7a226
kino6052 <kino6052@colorado.edu>
2016-02-09 20:35:26 -0700

    Restored Local Strategy

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -3,6 +3,12 @@
 angular.module('oxhnApp')
   .config(function($stateProvider) {
     $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: 'app/account/login/login.html',
+            controller: 'LoginController',
+            controllerAs: 'vm'
+        })
       .state('logout', {
         url: '/logout?referrer',
         referrer: 'main',
@@ -14,7 +20,20 @@ angular.module('oxhnApp')
           Auth.logout();
           $state.go(referrer);
         }
-      });
+         })
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'app/account/signup/signup.html',
+            controller: 'SignupController',
+            controllerAs: 'vm'
+        })
+        .state('settings', {
+            url: '/settings',
+            templateUrl: 'app/account/settings/settings.html',
+            controller: 'SettingsController',
+            controllerAs: 'vm',
+            authenticate: true
+  });
   })
   .run(function($rootScope) {
     $rootScope.$on('$stateChangeStart', function(event, next, nextParams, current) {
